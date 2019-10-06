235ca36785e706ef1dd7ddc3db99752b21e9f1ac
Darlan Alves <me@darlanalv.es>
2019-10-06 06:16:10 +0200

    fix(*): add interrupt partial example

--- interrupts/src/index.cpp
+++ interrupts/src/index.cpp
@@ -4,36 +4,51 @@ extern "C" {
 
 #include "index.h"
 #include "homebots.h"
+#include "ets_sys.h"
 #include "gpio.h"
 #include "osapi.h"
 
-static os_timer_t timer;
-
-void togglePin(void *arg) {
-  int state = GPIO_REG_READ(GPIO_OUT_ADDRESS) & BIT2;
-
-  os_printf("-- GPIO2: %d \r\n", state);
-
-  if (state > 0) {
-      gpio_output_set(0, BIT2, BIT2, 0);
-  } else {
-      gpio_output_set(BIT2, 0, BIT2, 0);
-  }
-}
+static os_timer_t interruptCallback;
+static uint32_t interrupt_reg = 0;
+static uint16_t gpioStatus = -1;
 
 void loop() {}
 
+void onButtonPress() {
+  if (gpioStatus & BIT2) {
+    os_printf("Press IO 2 %d", isHigh(2));
+  }
+
+  if (gpioStatus == 0) {
+    os_printf("Press IO 0 %d", isHigh(0));
+  }
+
+  os_printf("INTR %.8x", gpioStatus);
+}
+
+void onInterrupt(void*) {
+  ETS_GPIO_INTR_DISABLE();
+
+  uint16_t gpioStatus = GPIO_REG_READ(GPIO_STATUS_ADDRESS);
+  GPIO_REG_WRITE(GPIO_STATUS_W1TC_ADDRESS, gpioStatus);
+
+  os_timer_arm(&interruptCallback, 1, 0);
+
+  ETS_GPIO_INTR_ATTACH(onInterrupt, NULL);
+  ETS_GPIO_INTR_ENABLE();
+}
+
 void setup() {
-  gpio_init();
+  PIN_PULLUP_EN(PERIPHS_IO_MUX_GPIO2_U);
+  PIN_PULLUP_EN(PERIPHS_IO_MUX_GPIO0_U);
+  GPIO_DIS_OUTPUT(GPIO_ID_PIN(2));
 
-  //Set GPIO2 to output mode
-  PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO2_U, FUNC_GPIO2);
+  os_timer_setfn(&interruptCallback, (os_timer_func_t *)onButtonPress, NULL);
+  gpio_pin_intr_state_set(GPIO_ID_PIN(2), GPIO_PIN_INTR_POSEDGE);
 
-  //Set GPIO2 low
-  gpio_output_set(0, BIT2, BIT2, 0);
-
-  os_timer_setfn(&timer, (os_timer_func_t *)togglePin, NULL);
-  os_timer_arm(&timer, 1000, 1);
+  ETS_GPIO_INTR_DISABLE();
+  ETS_GPIO_INTR_ATTACH(onInterrupt, NULL);
+  ETS_GPIO_INTR_ENABLE();
 }
 
 #ifdef __cplusplus
